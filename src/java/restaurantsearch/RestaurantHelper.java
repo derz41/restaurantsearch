9c6da102df657bafa8d08fa0ffc02784f10f23f6
derz41 <andersonm@chc.edu>
2017-02-27 20:36:40 -0500

    Restaurant controller, helper and xhtml started. Error when bringing up xhtml page

---
+++ src/java/restaurantsearch/RestaurantHelper.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurantsearch;
+
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Matt
+ */
+public class RestaurantHelper {
+    
+    Session session = null;
+    
+    public RestaurantHelper() {
+        try {
+            this.session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public int selectRestaurant(Restaurant a) {
+        int result = 0;
+        
+        String sql = "select * from restaurant where restaurant_name = :restName ";
+        
+        try {
+            //starting the transaction if one is not active
+            if (!this.session.getTransaction().isActive()){
+                session.beginTransaction();
+            }
+            
+            //creaing query that can be executed
+            SQLQuery q = session.createSQLQuery(sql);
+            // associating actor POJO and table with the query
+            q.addEntity(Restaurant.class);
+            
+            //binding the values to the placeholders in the query
+            q.setParameter("restName", a.getRestaurantName());
+            
+            //executing the query
+            result = q.executeUpdate();
+            
+            //commiting the query to the database
+            session.getTransaction().commit();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return result;
+    }
+    
+}
